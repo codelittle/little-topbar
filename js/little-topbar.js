0053d73d85348d6b4f3466f376f12da5ed408536
Igor Putina <igor@wearepropeople.com>
2014-05-13 17:05:26 +0300

    Added some minimal responsive functionality

--- js/little-topbar.js
+++ js/little-topbar.js
@@ -1,4 +1,16 @@
 (function() {
-
+  $(document).ready(function() {
+    var collapse_link, expand_class, expand_link, topbar;
+    topbar = $("nav.little-topbar");
+    expand_link = $(".little-toggle a.expand");
+    collapse_link = $(".little-toggle a.collapse");
+    expand_class = "expanded";
+    expand_link.click(function() {
+      topbar.addClass(expand_class);
+    });
+    collapse_link.click(function() {
+      topbar.removeClass(expand_class);
+    });
+  });
 
 }).call(this);
--- src/main.coffee
+++ src/main.coffee
@@ -1,3 +1,16 @@
 # Little Topbar
 # A tiny responsive navigation library
 
+$(document).ready ->
+	topbar = $ "nav.little-topbar"
+	expand_link = $ ".little-toggle a.expand"
+	collapse_link = $ ".little-toggle a.collapse"
+	expand_class = "expanded"
+
+	expand_link.click ->
+		topbar.addClass expand_class
+		return
+	collapse_link.click ->
+		topbar.removeClass expand_class
+		return
+	return
